minetest/minetest

Add per-node climb speed modifier

src/client/localplayer.cpp
@@ -286,6 +286,9 @@ void LocalPlayer::move(f32 dtime, Environment *env, f32 pos_max_d,
 	} else {
 		is_climbing = (nodemgr->get(node.getContent()).climbable ||
 			nodemgr->get(node2.getContent()).climbable) && !free_move;
+		if (is_climbing) {
+			node_climb_factor = nodemgr->get(node.getContent()).climb_factor;
+		}
 	}
 
 	/*
@@ -557,7 +560,7 @@ void LocalPlayer::applyControl(float dtime, Environment *env)
 				speedV.Y = -movement_speed_walk;
 				swimming_vertical = true;
 			} else if (is_climbing && !m_disable_descend) {
-				speedV.Y = -movement_speed_climb * physics_override.speed_climb;
+				speedV.Y = -movement_speed_climb * physics_override.speed_climb * node_climb_factor;
 			} else {
 				// If not free movement but fast is allowed, aux1 is
 				// "Turbo button"
@@ -593,9 +596,9 @@ void LocalPlayer::applyControl(float dtime, Environment *env)
 				swimming_vertical = true;
 			} else if (is_climbing && !m_disable_descend) {
 				if (fast_climb)
-					speedV.Y = -movement_speed_fast;
+					speedV.Y = -movement_speed_fast * node_climb_factor;
 				else
-					speedV.Y = -movement_speed_climb * physics_override.speed_climb;
+					speedV.Y = -movement_speed_climb * physics_override.speed_climb * node_climb_factor;
 			}
 		}
 	}
@@ -647,7 +650,7 @@ void LocalPlayer::applyControl(float dtime, Environment *env)
 			if (fast_climb)
 				speedV.Y = movement_speed_fast;
 			else
-				speedV.Y = movement_speed_climb * physics_override.speed_climb;
+				speedV.Y = movement_speed_climb * physics_override.speed_climb * node_climb_factor;
 		}
 	}
 

AlphaFleet2024/MineQuest

src/client/localplayer.cpp
@@ -333,6 +333,9 @@ void LocalPlayer::move(f32 dtime, Environment *env, f32 pos_max_d,
 	} else {
 		is_climbing = (nodemgr->get(node.getContent()).climbable ||
 			nodemgr->get(node2.getContent()).climbable) && !free_move;
+		if (is_climbing) {
+			node_climb_factor = nodemgr->get(node.getContent()).climb_factor;
+		}
 	}
 
 	/*
@@ -610,7 +613,7 @@ void LocalPlayer::applyControl(float dtime, Environment *env)
 				speedV.Y = -speed_walk;
 				swimming_vertical = true;
 			} else if (is_climbing && !m_disable_descend) {
-				speedV.Y = -movement_speed_climb * physics_override.speed_climb;
+				speedV.Y = -movement_speed_climb * physics_override.speed_climb * node_climb_factor;
 			} else {
 				// If not free movement but fast is allowed, aux1 is
 				// "Turbo button"
@@ -646,9 +649,9 @@ void LocalPlayer::applyControl(float dtime, Environment *env)
 				swimming_vertical = true;
 			} else if (is_climbing && !m_disable_descend) {
 				if (fast_climb)
-					speedV.Y = -speed_fast;
+					speedV.Y = -speed_fast * node_climb_factor;
 				else
-					speedV.Y = -movement_speed_climb * physics_override.speed_climb;
+					speedV.Y = -movement_speed_climb * physics_override.speed_climb * node_climb_factor;
 			}
 		}
 	}
@@ -701,7 +704,7 @@ void LocalPlayer::applyControl(float dtime, Environment *env)
 			if (fast_climb)
 				speedV.Y = speed_fast;
 			else
-				speedV.Y = movement_speed_climb * physics_override.speed_climb;
+				speedV.Y = movement_speed_climb * physics_override.speed_climb * node_climb_factor;
 		}
 	}
 